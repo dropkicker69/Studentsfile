c7f1b0f3245a4657a7115516667591f6d61233cb
dropkicker69 <124162182+dropkicker69@users.noreply.github.com>
2023-02-02 00:02:43 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim marks As Integer
+        FileOpen(1, My.Application.Info.DirectoryPath & "\StFile.txt", OpenMode.Append)
+        For x = 1 To 10
+            Console.Write(" Enter Name : ")
+            name = Console.ReadLine()
+            Console.Write(" Enter marks : ")
+            marks = Console.ReadLine()
+            If marks < 30 Then
+                WriteLine(1, name)
+            End If
+        Next
+        FileClose(1)
+
+    End Sub
+
+End Module
